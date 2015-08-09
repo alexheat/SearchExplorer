dd27b70cc3bb02672e2e0e5a78408bf5b08f84de
Alex Heaton <aheaton@alexpana.fareast.corp.microsoft.com>
2015-08-09 10:35:38 +0900

    First Checkin

---
+++ server.R
@@ -0,0 +1,68 @@
+library(shiny)
+library(UsingR)
+library("ggplot2")
+library("dplyr")
+
+keywords <- read.csv("keywords.csv")
+keywords <- keywords[-1,]
+keywords$Clicks <- keywords$Clicks/1000
+keywords$Revenue <- keywords$Clicks * keywords$CPC
+
+shinyServer(
+  function(input, output) {
+    output$newHist <- renderPlot({
+      lower <- input$range[1]
+      upper <- input$range[2]
+      
+      # with input$radio, e.g.
+      
+      if (input$radio==1) { 
+        keywords <- arrange(keywords, CPC)
+        keywords$Keyword <- revFactor(factor(keywords$Keyword, levels = keywords$Keyword))
+        keywords <- arrange(keywords, desc(CPC))
+        chartValue <- "CPC"
+        chartTitle <- "Cost per Click"
+        yaxisLabel <- "Cost per Click (GBP)"
+        
+      } else if (input$radio==2) {
+        
+        keywords <- arrange(keywords, Clicks)
+        keywords$Keyword <- revFactor(factor(keywords$Keyword, levels = keywords$Keyword))
+        keywords <- arrange(keywords, desc(Clicks))
+        chartValue <- "Clicks"
+        chartTitle <- "Total Clicks"
+        yaxisLabel <- "Total Clicks (thousands)"
+        
+      } else if (input$radio==3) {
+        keywords <- arrange(keywords, CTR)
+        keywords$Keyword <- revFactor(factor(keywords$Keyword, levels = keywords$Keyword))
+        keywords <- arrange(keywords, desc(CTR))
+        chartValue <- "CTR"
+        chartTitle <- "Click-through Rate"
+        yaxisLabel <- "Click-through Rate"
+        
+      }
+        else if (input$radio==4) {
+        keywords <- arrange(keywords, Revenue)
+        keywords$Keyword <- revFactor(factor(keywords$Keyword, levels = keywords$Keyword))
+        keywords <- arrange(keywords, desc(Revenue))
+        chartValue <- "Revenue"
+        chartTitle <- "Total Revenue"
+        yaxisLabel <- "Total Revenue GPB (millions)"
+      }
+      
+      ggplot(data=keywords[upper:lower,], aes_string(x="Keyword", y=chartValue, fill=chartValue)) +
+        geom_bar(stat="identity") +
+        ggtitle(chartTitle) +
+        ylab(yaxisLabel) + xlab(NULL) +
+        #scale_y_sqrt(chartValue) +
+        theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5, size = 12))      
+
+        })
+    
+  }
+)
+
+
+# You can access the value of the widget with input$action, e.g.
+# output$value <- renderPrint({ input$action })
---
+++ ui.R
@@ -0,0 +1,26 @@
+library(shiny)
+
+shinyUI(pageWithSidebar(
+  headerPanel(""),
+  sidebarPanel(
+    sliderInput("range", "Select range of keywords to view:",
+                min = 1, max = 500, value = c(1,25)),
+    radioButtons("radio", "Select value to plot:",
+                 choices = list("Cost per click" = 1, "Clicks" = 2, "Click-through-rate" = 3,  
+                                "Total revenue" = 4), selected = 1),
+    radioButtons("radio2", "Select secondary value for bar color:",
+                 choices = list("Cost per click" = 1, "Clicks" = 2, "Click-through-rate" = 3,  
+                                "Total revenue" = 4), selected = 1),
+    submitButton(text = "Update Chart", icon = NULL)
+    
+  ),
+  mainPanel(
+    h1("Search Keywords Explorer"),
+    HTML("Use this tool to explore performance of  paid search terms. Data is from the "),
+    tags$a(href="https://code.google.com/p/open-advertising-dataset/", "Open Advertising Dataset"),
+    HTML("July 2012 - February 2013."),
+    plotOutput('newHist'),
+    tags$p(""),
+    tags$b("About this application:")
+  )
+))
